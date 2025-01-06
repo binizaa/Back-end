558b746586494beee260b5cb34a1623c389fd3ff
binizaa <binizacruuiz@gmail.com>
2025-01-06 10:01:58 -0600

    Ignore unnecessary folders except for Entrega1

--- Clases/Class6/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.end('My first hello word!!')
-})
-
-server.listen(8080, () => {
-    console.log('Listening on port 8080')
-})
